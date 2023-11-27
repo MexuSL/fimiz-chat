aa0a7e5f6982a4d418e1cafd706ba48ce1ce1f5a
MexuSL <mexu.company@gmail.com>
2023-11-27 13:49:16 +0000

    chore:Added CI and CD,modify doccker-compose file,setenv,and docker build setup

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,23 @@
+# Use an official Node.js runtime as a base image
 FROM node:alpine
-RUN MKDIR /app
+
+# Set the working directory in the container
 WORKDIR /app
+
+# Copy package.json and package-lock.json to the working directory
 COPY package*.json ./
+
+# Install app dependencies
 RUN npm install
+
+# Copy the application code into the container
 COPY . .
+
+# Build the TypeScript code
 RUN npm run build
-EXPOSE 6000
-CMD [ "node","./dist/src/index.js"]
+
+# Expose the port the app runs on
+EXPOSE 8080
+
+# Define the command to run your app
+CMD ["npm", "start"]
--- setenv.sh
+++ setenv.sh
@@ -1,13 +1,23 @@
-export PORT=6000
+#!/bin/bash
+
+echo "Setting up environment variables"
+
+export PORT=8080
 export MAIL_PORT=465
-export MAIL_HOST="smtp.mail.yahoo.com"
-export MAIL_USER="mexu.sl@yahoo.com"
-export MAIL_PASS="rdooytekacavmmnn"
-export PG_DB_PASSWORD="Mexu2023"
-export PG_DB_USERNAME="mexu"
-export PG_DB_HOST=".ccn2o7dzia1z.us-east-1.rds.amazonaws.com"
-export PG_DB_NAME="db"
-export API_ACCESS_KEY="mexu2023medishk2"
-export SMS_API_KEY="9d035dbc"
-export SMS_SECRET_KEY="fUr53lsNeWpUL1iV"
-export ENV="production"
+export MAIL_HOST=smtp.mail.yahoo.com
+export MAIL_USER=mexu.sl@yahoo.com
+export MAIL_PASS=rdooytekacavmmnn
+export PG_DB_PASSWORD=Mexu2023
+export PG_DB_USERNAME=fimiz
+export PG_DB_HOST=fimiz-blog-db.ccn2o7dzia1z.us-east-1.rds.amazonaws.com
+export PG_DB_NAME=fimiz-blog-db
+# export API_ACCESS_KEY=mexu2023medishk2
+# export SMS_API_KEY=9d035dbc
+# export SMS_SECRET_KEY=fUr53lsNeWpUL1iV
+export ENV=production
+
+echo "Done setting up environment variables"
+
+echo "PORT :$PORT"
+
+
